337762a2e5f6b2d05a8f50897e8e53d7692a3716
Vlad Garkusha <vladyslav.garkusha@origyn.ch>
2022-03-25 11:18:45 +0200

    updated dfx json

--- rs/rosetta-api/ledger_canister/scripts/deploy.sh
+++ rs/rosetta-api/ledger_canister/scripts/deploy.sh
@@ -2,14 +2,20 @@
 
 set -o xtrace
 
-dfx identity new admin
+dfx identity new ic_admin
+
+MINT_ACC=$(dfx ledger account-id)
+
+mkdir ~/.config/dfx/identity/admin/
 
 echo ${DFX_IDENTITY} > ~/.config/dfx/identity/admin/identity.pem
 
 dfx identity use admin
 
+LEDGER_ACC=$(dfx ledger account-id)
+
 dfx identity whoami
 
 dfx identity set-wallet rwlgt-iiaaa-aaaaa-aaaaa-cai
 
-dfx deploy --network local
+dfx deploy ledger --argument "record {minting_account = \"${MINT_ACC}\"; initial_values = vec { record { \"${LEDGER_ACC}\"; record { e8s = 18446744073709551615 } } }; max_message_size_bytes = null; transaction_window = null; archive_options = null; send_whitelist = vec {}}" --network=local
